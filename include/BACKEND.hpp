02c79ae2c7861ea38d29151d47e0e8333a15c48c
Jacob August Davison <daviso53@msu.edu>
2021-11-04 14:00:50 -0400

    parent header template for flow backends

---
+++ include/BACKEND.hpp
@@ -0,0 +1,37 @@
+#ifndef BACKEND_HPP_
+#define BACKEND_HPP_
+
+#include <boost/numeric/ublas/vector.hpp>
+#include "imsrg_utils.hpp"
+
+class Backend {
+
+
+public:
+    
+    virtual double flow_0b(boost::numeric::ublas::vector<double> &occA,
+                           boost::numeric::ublas::vector<double> &occD,
+                           boost::numeric::ublas::vector<double> &f, 
+                           boost::numeric::ublas::vector<double> &Gamma, 
+                           boost::numeric::ublas::vector<double> &eta1b, 
+                           boost::numeric::ublas::vector<double> &eta2b) = 0;
+
+    virtual boost::numeric::ublas::vector<double> flow_1b(boost::numeric::ublas::vector<double> &occA,
+                                                          boost::numeric::ublas::vector<double> &occC,                                                          
+                                                          boost::numeric::ublas::vector<double> &f, 
+                                                          boost::numeric::ublas::vector<double> &Gamma, 
+                                                          boost::numeric::ublas::vector<double> &eta1b, 
+                                                          boost::numeric::ublas::vector<double> &eta2b) = 0;
+
+    virtual boost::numeric::ublas::vector<double> flow_2b(boost::numeric::ublas::vector<double> &occA,
+                                                          boost::numeric::ublas::vector<double> &occB,
+                                                          boost::numeric::ublas::vector<double> &f, 
+                                                          boost::numeric::ublas::vector<double> &Gamma, 
+                                                          boost::numeric::ublas::vector<double> &eta1b, 
+                                                          boost::numeric::ublas::vector<double> &eta2b) = 0;
+
+
+};
+
+
+#endif
